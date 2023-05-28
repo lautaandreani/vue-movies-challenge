5f169800c45e8af31b2a098b24443c78a31cb501
lautaro <lautaroandreani2016@gmail.com>
2023-05-27 22:58:40 -0300

    styles: min height container

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
       <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" type="movies" />
     </div>
 
-    <div class="w-full overflow-x-auto flex bg-soft_gray p-6 gap-10">
+    <div class="w-full overflow-x-auto flex bg-soft_gray p-6 gap-10 min-h-[35rem]">
       <MovieCard v-for="tvShow in tvShows" :key="tvShow.id" :movie="tvShow" type="tv" />
     </div>
   </aside>
